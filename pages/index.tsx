8d5af9c2cf179530ff302eaeb7b0fe510b3d8cb3
damon_gege <z3439119@gmail.com>
2023-05-12 21:13:41 +1000

    update UI and docs

--- pages/index.tsx
+++ pages/index.tsx
@@ -257,13 +257,12 @@ export default function Home() {
                       </div>
                     </div>
                     <div className="chat-bubble bg-slate-50 text-slate-900">
-                      <div className=" flex flex-row justify-between gap-2">
-                        <ReactMarkdown linkTarget="_blank">
+                      <div className=" flex flex-row">
+                        <p>
                           I am hustling to solve the question,please wait...
-                        </ReactMarkdown>
                         <svg
                           aria-hidden="true"
-                          className="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-slate-700 dark:fill-gray-300"
+                          className="inline w-4 h-4 mx-2 text-gray-200 animate-spin dark:text-gray-600 fill-slate-700 dark:fill-gray-300"
                           viewBox="0 0 100 101"
                           fill="none"
                           xmlns="http://www.w3.org/2000/svg"
@@ -277,6 +276,7 @@ export default function Home() {
                             fill="currentFill"
                           />
                         </svg>
+                        </p>
                       </div>
                     </div>
                   </div>
